d9dd220eba0f17e66cdec354d26bfa4d768abd47
Advik Singh <singhinalaska@gmail.com>
2025-09-10 19:29:58 -0500

    Edited db.go file to add pure Go sqlite driver Also fixed error that was not saving changes to the database

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,10 +1,28 @@
 package db
 
 import (
+	"database/sql"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	_ "modernc.org/sqlite" // import the pure-Go SQLite driver
 )
 
+// Open opens a GORM DB connection using pure-Go SQLite.
 func Open(path string) (*gorm.DB, error) {
-	return gorm.Open(sqlite.Open(path), &gorm.Config{})
+	// Create a database/sql DB using modernc.org/sqlite
+	sqlDB, err := sql.Open("sqlite", path)
+	if err != nil {
+		return nil, err
+	}
+
+	// Pass the *sql.DB to GORM
+	gdb, err := gorm.Open(sqlite.New(sqlite.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return gdb, nil
 }
